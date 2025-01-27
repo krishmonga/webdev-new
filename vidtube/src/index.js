8ad5f60fd4ab40de1b7d9474cc4fd0082dfb7631
Your Name <your-email@example.com>
2025-01-27 10:33:59 +0530

    will connect databases soon

--- vidtube/src/index.js
+++ vidtube/src/index.js
@@ -1,5 +1,10 @@
- import {app} from './app.js'
- const port=7000;
+import dotenv from "dotenv"
+import {app} from './app.js'
+
+dotenv.config({
+   path  :"./.env"
+})
+ const port=process.env.PORT||7000;
  app.listen(port,()=>{
     console.log(`server is running on port ${port} ` );
     
